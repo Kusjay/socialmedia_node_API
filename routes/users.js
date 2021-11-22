a08d88684fcac84f7b0172c8da349ed86c76348e
Kelechi <kelechiuma@gmail.om>
2021-11-22 14:48:27 +0100

    Unfollow a user endpoint

--- routes/users.js
+++ routes/users.js
@@ -77,5 +77,27 @@ router.put('/:id/follow', async (req, res) => {
 });
 
 // Unfollow a user
+router.put('/:id/unfollow', async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+
+      if (user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $pull: { followers: req.body.userId } });
+        await currentUser.updateOne({
+          $pull: { following: req.params.id },
+        });
+        res.status(403).json('User has been unfollowed');
+      } else {
+        res.status(403).json('You do not follow this user');
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json('You cannot ufollow yourself');
+  }
+});
 
 module.exports = router;
