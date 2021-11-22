14b17918997fabd4fe76290ced251a0d4272078c
Kelechi <kelechiuma@gmail.om>
2021-11-22 23:31:42 +0100

    Update and Delete a post endpoint

--- routes/posts.js
+++ routes/posts.js
@@ -29,6 +29,20 @@ router.put('/:id', async (req, res) => {
 });
 
 // Delete a post
+router.delete('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      res.status(200).json('Post deleted successfully');
+    } else {
+      res.status(403).json('You can only delete your post');
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // Like a post
 // Get a post
 // Get timeline posts
