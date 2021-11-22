0cc952daccae60f872e389570aa6eaf0f597194f
Kelechi <kelechiuma@gmail.om>
2021-11-22 11:41:13 +0100

    Added user registration endpoint fix

--- routes/auth.js
+++ routes/auth.js
@@ -6,12 +6,12 @@ router.post('/register', async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.username,
+    password: req.body.password,
   });
 
   try {
     const user = await newUser.save();
-    res.status(200).json(user);
+    res.status(200).json();
   } catch (err) {
     console.log(err);
   }
