58be54077b2700a6db64bb51a403d1ae87441783
Kelechi <kelechiuma@gmail.om>
2021-11-22 14:35:02 +0100

    Follow a user endpoint fix

--- routes/users.js
+++ routes/users.js
@@ -61,7 +61,7 @@ router.put('/:id/follow', async (req, res) => {
 
       if (!user.followers.includes(req.body.userId)) {
         await user.updateOne({ $push: { followers: req.body.userId } });
-        await currentUser.updateOne({ $push: { followings: req.body.userId } });
+        await currentUser.updateOne({ $push: { following: req.body.userId } });
         res.status(403).json('User has been followed');
       } else {
         res.status(403).json('You already followed this user');
