cc21442ddb9019da256aae4d7c9a8ccf3fd16c12
Kelechi <kelechiuma@gmail.om>
2021-11-22 23:46:37 +0100

    Like and dislike a post endpoint

--- routes/posts.js
+++ routes/posts.js
@@ -43,7 +43,22 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
-// Like a post
+// Like / dislike a post
+router.put('/:id/like', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json('Post liked');
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json('Post disliked');
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // Get a post
 // Get timeline posts
 
