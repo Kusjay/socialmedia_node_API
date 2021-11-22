8960b2e7301c48c07006726bb4300a190f042885
Kelechi <kelechiuma@gmail.om>
2021-11-22 11:54:32 +0100

    Encrypting the user password when registering

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,24 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 // Register users
 router.post('/register', async (req, res) => {
-  const newUser = new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-  });
-
   try {
+    // Generate hashed password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    // Create new user
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    // Save user and return response
     const user = await newUser.save();
-    res.status(200).json();
+    res.status(200).json(user);
   } catch (err) {
     console.log(err);
   }
